a57732718d4bd0e688e72df9e24e7e915849670f
gabquepa <gaby_quesada@hotmail.com>
2014-07-12 11:30:52 -0600

    showing forum moderator and invited students -

--- js/controllers.js
+++ js/controllers.js
@@ -11,18 +11,22 @@ app.controller('ForumController', function($scope){
 	this.enableTxt  = function(){
 		if($scope.enable = true && $(".main-forum").attr('disabled')==='disabled'){
 			$scope.enable = false;
-			$(".main-forum").attr('disabled',false);	
+			$('.main-forum').attr('disabled',false);	
 		}
 		else{
 			$scope.enable = true;
-			$(".main-forum").attr('disabled',true);	
+			$('.main-forum').attr('disabled',true);	
 		}
 		
 	};
 
 	this.addToForum = function(){
-		
-		
+		var invitados = $('.invitados').val().split(', ');
+		$('#moderadorDisplay').text($('.moderador').val());
+		$('#moderadorDisplay').append('<span id="delete" class="glyphicon glyphicon-remove"></span>');
+		for (var i = invitados.length - 1; i >= 0; i--) {
+			$('#invitadosDisplay').append('<li>'+invitados[i]+' <span id="delete" class="glyphicon glyphicon-remove"></span></li>')
+		};
 	};
 	
 });	
