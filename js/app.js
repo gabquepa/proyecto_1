6bf379f533e5817d5a311298557a38b848813724
alezchin <alezchin@hotmail.com>
2014-07-12 13:32:50 -0600

    Test1-TABS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 (function(){
 	var app = angular.module('myApp',['partials-project', 'controllers-project']);
+
+
+
+	app.controller('configController', function($scope) {
+
+		  $scope.mostrarTabUsuario = false;
+
+		  $scope.mostrarCrearUsuario = false;
+		  $scope.mostrarModificarUsuario = false;
+		  $scope.mostrarInhabilitarUsuario = false;
+	});
+
 	
 
 
