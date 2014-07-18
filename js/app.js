aa687cf69294a6a397150336cc99e8d9cea2fbc0
alezchin <alezchin@hotmail.com>
2014-07-18 00:00:48 -0600

    Update de Configuration

--- js/app.js
+++ js/app.js
@@ -8,20 +8,35 @@
 		 this.usuarios = arregloUsuarios;
 		  this.carreras = arregloCarreras;
 		  this.miCarrera = {};
-		  this.miCurso = {};
+		  this.miCarreraCC = {};
+		  this.miCursoCC = {};
+		  this.miCarreraMC = {};
+		  this.miCursoMC = {};
 		  this.indextemp = 0;
-		  this.buscarIndex= function(){
 
-		  	for(i=0;i<this.carreras.length;i++){
 
-		  		if(this.carreras[i].nombre === this.miCarrera.nombre){
-		  		
-		  			this.carreras[i].cursos.push(this.miCurso);
-		  		}
-		  	}
 
-		  this.miCarrera = {};
-		  }
+		this.addCarrera = function(){
+			
+			this.miCarrera.estado = 'activo';
+			this.miCarrera.cursos = {};
+			 console.log(this.miCarrera.estado);
+			console.log(this.miCarrera.cursos);
+			this.carreras.push(this.miCarrera);
+			
+			console.log(this.carreras[3].estado);
+			console.log(this.carreras[3].cursos);
+			 
+			 // console.log(this.carrera[3].estado);
+			 // console.log(this.carrera[3].cursos);
+			 // console.log(this.carreras[0].nombre);
+			 // console.log(this.carreras[1].nombre);
+			 // console.log(this.carreras[2].nombre);
+			 // console.log(this.carreras[3].nombre);
+			
+			 this.miCarrera = {};	
+		};
+
 
 		  this.modifCarrera= function(){
 
@@ -52,6 +67,75 @@
 		  		$('#codCarrera').val("");
 		  		$('#nomCarrera').val("");
 				this.miCarrera = {};
+				this.indextemp="";
+		  }
+
+		 
+
+
+		  this.crearCurso= function(){
+
+            
+		  	for(var i=0;i<this.carreras.length;i++){
+              
+		  		if(this.carreras[i].nombre === this.miCarreraCC.nombre){
+		  		    // console.log(this.miCursoCC.nombreCurso);
+		  		    // console.log(this.miCursoCC.idCurso);
+		  		    // console.log(this.miCursoCC);
+		  		    // console.log(this.carreras[i].cursos);
+		  			//this.carreras[i].cursos.push(this.miCursoCC);
+		  			this.carreras[i].cursos.nombreCurso=this.miCursoCC.nombreCurso;
+		  			this.carreras[i].cursos.idCurso=this.miCursoCC.idCurso;
+		  		    console.log(this.carreras[i].cursos);
+		  		}
+		  	}
+		 
+
+		  }
+
+
+ 			this.getCurso = function(){
+ 					console.log(this.miCarreraMC.cursos);
+		  		return this.miCarreraMC.cursos;
+		 	 }
+
+		   this.modifCurso= function(){
+		   			console.log('HHH');
+		  	
+		  	for(i=0;i<this.carreras.length;i++){
+
+		  		if(this.carreras[i].nombre === this.miCarreraMC.nombre){
+		  		
+		  			// this.carreras[i].cursos.push(this.miCurso);
+		  			$('#modifIdCurso').val(this.carreras[i].codigo);
+		  			$('#modifNombreCurso').val(this.carreras[i].nombre);
+		  			this.indextemp=i;
+
+		  		}
+		  	}
+
+		  //this.miCarrera = {};
+		  }
+
+		  this.actualizarCurso= function(){
+
+		  	for(i=0;i<this.carreras[this.indextemp].cursos.length;i++){
+
+		  		if(this.carreras[this.indextemp].cursos[i].nombreCurso === this.miCursoMC.nombreCurso){
+		  			this.carreras[this.indextemp].cursos[i].nombreCurso= this.miCursoMC.nombreCurso;	
+		  		}
+		  		if(this.carreras[this.indextemp].cursos[i].idCurso === this.miCursoMC.idCurso){
+		  			this.carreras[this.indextemp].cursos[i].idCurso= this.miCursoMC.idCurso;	
+		  		}
+		  	 }
+
+		  	   $('#modifIdCurso').val("");
+		  	    $('#modifNombreCurso').val("");
+				
+				this.miCarreraMC = {};
+				this.indextemp="";
+				this.miCursoMC={};
+
 		  }
 
 		  
