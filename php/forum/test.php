f7534d0acf0aa0f26076c0bf3130da1a36caf969
Keilyn Sibaja Venegas <keilynsibajavenegas@gmail.com>
2014-08-11 16:03:26 -0600

    test forum

--- php/forum/test.php
+++ php/forum/test.php
@@ -1,8 +1,35 @@
 <?php 
-$conexion= mysql_connect ( ) or die ("no se puede conectar");
+$conexion= mysql_connect ("localhost", "root", "root") or die ("no se puede conectar");// direccion para encontrar y conectarse a la base de datos.
 
-mysql_select_db("Proyecto_1") or die ("no se puede conectar");
-print "estoy conectada";
+mysql_select_db("Proyecto_1") or die ("no se puede conectar");//selecciona la base de datos en donde vamos a hacer las llamadas
+
+$consulta = mysql_query ( "select * from foro", $conexion) or die ("fallo de la consulta"); // select donde llamo a mi tabla
+
+$consultaProfe = mysql_query ( "select * from usuario", $conexion) or die ("fallo de la consulta"); 
+
+$nfilas = mysql_num_rows ($consulta); //retorna las todas las filas de la tabla
+
+$nfilasProfe= mysql_num_rows ($consultaProfe); 
+
+for ($i=0; $i < $nfilas; $i++) { 
+	$fila = mysql_fetch_array ($consulta);// me devuelve la fila entera
+
+	$filaProfe = mysql_fetch_array ($consultaProfe);
+
+	print "<br>Titulo: " .$fila["titulo"];
+	print "<br>Texto: " .$fila["texto"];
+	print "<br>Fecha: " .$fila["fecha"];
+
+	if($fila["id_usuario"]==$filaProfe["id_usuario"]){
+		print "<br>Profe: " .$filaProfe["nombre"]." ".$filaProfe["apellido"]."<br>";
+	}
+	else{
+		print("<br>ERROR<br>");
+	}
+}
+
+
+print ("<br>estoy conectada");
 
 mysql_close($conexion);
 
