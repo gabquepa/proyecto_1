94bc7044cba13e8d9937d7e23428d519e829a4cb
gabquepa <gaby_quesada@hotmail.com>
2014-11-27 18:47:28 -0600

    archivos que faltan

--- php/document/subir.php
+++ php/document/subir.php
@@ -1,7 +1,7 @@
 <?php
 
 $archivo = $_FILES["archivo"] ['name'];
-$destino = "C:/xampp/htdocs/Proyecto_1/storage/".$archivo;
+$destino = "/Applications/MAMP/htdocs/Proyecto_1/storage/".$archivo;
 copy($_FILES["archivo"] ['tmp_name'],$destino);
 header ("Location: /Proyecto_1/documentos.html");
 
